418f4bd15adb0c8ad901efa344399112823ee370
SKHolmes <samualkholmes@gmail.com>
2016-05-12 02:06:59 +1200

    Added animated text not sure if we should keep it or not, could be nice.

--- app.js
+++ app.js
@@ -3,7 +3,27 @@
 
   var app = document.querySelector('#app');
   window.addEventListener('WebComponentsReady', function() {
-    
+    var animationArray = ['Buy!', 'Sell!', 'That\'s it!', 'No nonsense!', 'Free to use!'];
+    var animationTxt = document.getElementById('animationText');
+    var animationIndex = 0;
+    var animationDelay = 500;
+
+    app.animateText = function(){
+      animationTxt.innerHTML = animationArray[animationIndex];
+      animationTxt.className = 'animated fadeInUp';
+      animationIndex += 1;
+      if(animationIndex == animationArray.length){
+        animationIndex = 0;
+      }
+      setTimeout(function(){
+        animationTxt.className = 'animated fadeOutUp';
+        setTimeout(function(){
+          app.animateText();
+        }, 2000);
+      }, 3000);
+    }
+
+    app.animateText();
   });  
 })(document);
 
